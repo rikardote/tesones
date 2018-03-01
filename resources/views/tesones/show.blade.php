1c102d6af680b9490f42cbc1e9b0b4b0234a8618
rikardote <ricarmenta@gmail.com>
2018-03-01 19:34:37 +0000

    fix layout

--- resources/views/tesones/show.blade.php
+++ resources/views/tesones/show.blade.php
@@ -113,7 +113,7 @@
 
 		</tr>
 	@endforeach
-	@for($i=$cancelaciones->count(); $i<=16; $i++)
+	@for($i=$cancelaciones->count(); $i<=12; $i++)
 		<tr><td style="border:0px"; colspan="6">&nbsp;</td></tr>
 		@if	($i==8 && $cancelaciones->count()==null)
 			<tr><td style="border:0px;font-size:20px;text-shadow: 3px 2px #808080;" colspan="6">&nbsp;PAGADA EN SU TOTALIDAD</td></tr>
